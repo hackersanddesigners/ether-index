d0c0e5ec1491d35bdf938a50e4c4e6bde5ea8624
André Fincato <work@andrefincato.info>
2022-09-11 19:02:28 +0200

    server: sketching out

--- server.py
+++ server.py
@@ -7,18 +7,14 @@ from blacksheep.server.templating import use_templates
 from jinja2 import PackageLoader
 from jinja2 import Environment, FileSystemLoader
 
-app = Application()
 
-@app.route("/", methods=["GET", "POST"])
-async def main(req: Request):
-    """
-    if GET request return paginated list of pads;
-    if POST request use value sent by form to redirect to
-    that URL and create a new pad
-    """
+# app init
+app = Application(show_error_details=True, debug=True)
+
 # create view function to read template from root folder
 template_path = os.path.join(os.path.dirname(__file__), './')
 view = use_templates(app, loader=FileSystemLoader(template_path))
+
 def datetime_format(timestamp):
     # <https://stackoverflow.com/a/31548402>
     # divide by /1000 to convert from milliseconds to seconds
@@ -32,23 +28,83 @@ def datetime_format(timestamp):
     ts = ts.astimezone().strftime('%Y-%m-%d %H:%M:%S')
     
     return ts
+
+
+def chunks(lst, n):
+    "Yield successive n-sized chunks from lst."
+
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+async def t(req):
     
     if req.method == 'GET':
 
         """
         return list of all pads (padID)
         paginate through the list and fetch x-amount of pads each time
-        start with 100 (?)
+        start with PAGINATION_SIZE
         """
 
-        URL_all_pads = "%s/api/1.2.1/listAllPads?apikey=%s" % (os.getenv('EP_URL'), os.getenv('EP_API_KEY'))
-        r = requests.get(URL_all_pads)
+        ep_url = os.getenv('EP_URL')
+        ep_api_key = os.getenv('EP_API_KEY')
+        
+        URL_all_pads = "%s/api/1.2.1/listAllPads?apikey=%s" % (ep_url, ep_api_key)
+        
+        # use requests sessions to first fetch list of pads
+        # then go through the list and check if padText contains `__NOINDEX__`
+        
+        try:
+            s = requests.Session()
+            r = s.get(URL_all_pads)
+            
+            r.raise_for_status()
+            
+            res = r.json()
+            
+        except requests.exceptions.HTTPError as e:
+            print('cannot fetch resource', r.status_code, '\n', e)
+            
+            padlist_res = None
+            pads_count = 0
+            
+            
+        padlist_res = res['data']['padIDs']
+        pads_count = len(padlist_res)
 
-        return json(r.json())
+        pagination_size = int(os.getenv('PAGINATION'))
+        padlist_data = chunks(padlist_res, pagination_size)
 
-    elif req.method == 'POST':
+        # we have a list of chunk ranges [0-99], [100-99], ...
+        # the URL has a route to set the pagination
+        # we read from it and select the correct range size
+
+        padlist = list(padlist_data)
+
 
         return view('index', {"pads": pads,
                               "pagination": pagination,
                               "ep_url": ep_url})
+
+
+    elif req.method == 'POST':
+        
         return "will redirect to <url> from form etc."
+
+
+@app.route("/", methods=["GET", "POST"])
+async def page(req: Request):
+    """
+    if GET request return paginated list of pads;
+    if POST request use value sent by form to redirect to
+    that URL and create a new pad
+    """
+    
+    return await t(req)
+
+
+@app.route("/page/<id>", methods=["GET", "POST"])
+async def subpage(req: Request):
+
+    return await t(req)
