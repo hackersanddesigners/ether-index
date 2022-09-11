2b534b409684f648505e128f0813ba45ed98e9c7
André Fincato <work@andrefincato.info>
2022-09-11 19:00:14 +0200

    server: add template

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 load_dotenv()
 import requests
 from blacksheep import Application, Request, json, redirect
+from blacksheep.server.templating import use_templates
+from jinja2 import PackageLoader
+from jinja2 import Environment, FileSystemLoader
 
 app = Application()
 
@@ -13,6 +16,22 @@ async def main(req: Request):
     if POST request use value sent by form to redirect to
     that URL and create a new pad
     """
+# create view function to read template from root folder
+template_path = os.path.join(os.path.dirname(__file__), './')
+view = use_templates(app, loader=FileSystemLoader(template_path))
+def datetime_format(timestamp):
+    # <https://stackoverflow.com/a/31548402>
+    # divide by /1000 to convert from milliseconds to seconds
+    ts = timestamp / 1000
+    
+    # <https://stackoverflow.com/a/37188257>
+    ts = datetime.datetime.utcfromtimestamp(ts)
+    
+    # <https://stackoverflow.com/a/46339491>
+    ts = ts.replace(tzinfo=datetime.timezone.utc)
+    ts = ts.astimezone().strftime('%Y-%m-%d %H:%M:%S')
+    
+    return ts
     
     if req.method == 'GET':
 
@@ -29,4 +48,7 @@ async def main(req: Request):
 
     elif req.method == 'POST':
 
+        return view('index', {"pads": pads,
+                              "pagination": pagination,
+                              "ep_url": ep_url})
         return "will redirect to <url> from form etc."
